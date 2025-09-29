de201b182ece0001105d5ed7e9d95cc0a8a469c3
Змеев Илья <zmeev.i.v@edu.mirea.ru>
2025-09-29 14:49:53 +0300

    fixed database url

--- database/connection.js
+++ database/connection.js
@@ -1,16 +1,16 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-
+    connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false // обязательно для Render!
+    },
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
 });
+
+module.exports = pool;
 
 pool.on('connect', () => {
     console.log('✅ Подключение к PostgreSQL установлено');
